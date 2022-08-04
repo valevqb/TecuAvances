69de08b8074c21275f3ebcb7de4bd70617620e5d
Valeria Quesada <vale.vqb03@gmail.com>
2022-08-04 16:55:32 -0600

    Código mejorado

--- finalReplace.py
+++ finalReplace.py
@@ -8,7 +8,8 @@ with open('data.json',encoding="utf-8") as file: #abre el archivo json con tilde
     data = json.load(file);
 
     for neutral in data['datos']: #agrega los datos
-        dictionary[neutral['original']] = neutral['replacement'];
+        for abc in neutral:
+            dictionary = dict (dictionary,**neutral[abc])
 
 texts = open ('prueba.txt', 'r');
 
@@ -17,7 +18,8 @@ contents = texts.read(); #contenido del texto
 for word in contents.split(): #recorrer texto por palabras
     for key in dictionary: #recorrer diccionario
         if(word[0:len(word)-2] == key): #si la palabra es igual a la llave
-            contents = contents.replace(word,dictionary[key]);
+            lists = dictionary[key]
+            contents = contents.replace(word,lists[0]);
 
 texts.close;
 
