e66ca4e7a78f3801c769cb1c6ab25af30da5ac25
Valeria Quesada <vale.vqb03@gmail.com>
2022-08-05 19:56:33 -0600

    Arreglos puntuaciones

--- finalReplace.py
+++ finalReplace.py
@@ -15,11 +15,24 @@ texts = open ('prueba.txt', 'r');
 
 contents = texts.read(); #contenido del texto
 
-for word in contents.split(): #recorrer texto por palabras
+#verifica contenido y elimina el reemplazo en donde no hay letras
+def isLetter(word):
+    counter = 0;
+    while(counter != len(word)):
+        if(word[counter].isalpha() == False):
+            return word[0:counter];
+        counter += 1;
+    return word;
+
+
+for word in contents.split(' '): #recorrer texto por palabras
+    print(word);
+    word2 = isLetter(word);
+    print(word2)
     for key in dictionary: #recorrer diccionario
-        if(word[0:len(word)-2] == key): #si la palabra es igual a la llave
+        if(word2[0:len(word2)-2] == key and word2[0:len(word2)-2] != ""): #si la palabra es igual a la llave
             lists = dictionary[key]
-            contents = contents.replace(word,lists[0]);
+            contents = contents.replace(word2,lists[0]);
 
 texts.close;
 
